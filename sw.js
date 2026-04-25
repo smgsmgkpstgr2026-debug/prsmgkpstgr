434c560176f500e60901d5155d0ef9ede144cb66
smgsmgkpstgr2026-debug <smgsmgkpstgr.2026@gmail.com>
2026-04-25 10:32:41 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Service Worker untuk PWA - Inventaris GSM
+const CACHE_NAME = 'inv-gsm-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
